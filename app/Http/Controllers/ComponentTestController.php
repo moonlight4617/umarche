19dbf11a413f65b9eabc30a318ab98af6698d117
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-08-26 14:53:40 +0900

    変数をコンポーネントに受けわたす

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -6,11 +6,14 @@ use Illuminate\Http\Request;
 
 class ComponentTestController extends Controller
 {
-    public function show1() {
-        return view('component.test1');
+    public function show1()
+    {
+        $message = 'メッセージ';
+        return view('component.test1', compact('message'));
     }
 
-    public function show2() {
+    public function show2()
+    {
         return view('component.test2');
     }
 }
