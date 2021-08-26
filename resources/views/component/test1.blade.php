01ac5aebb1331a3c6c70bf947e4c78f6c1d16d31
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-08-26 15:09:08 +0900

    propsを使ったコンポーネントの受け渡し

--- resources/views/component/test1.blade.php
+++ resources/views/component/test1.blade.php
@@ -2,4 +2,5 @@
   <x-slot name="header">ヘッダー</x-slot>
 コンポーネントテスト1
   <x-tests.card title="タイトル" content="本文" :message="$message" />
+  <x-tests.card title="タイトル2" />
 </x-tests.app>
--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,6 +1,12 @@
+@props([
+  'title',
+  'message' => '初期値メッセージ',
+  'content' => '初期値本文'
+])
+
 <div class="border-2 shadow-md w-1/4 p-2">
   <div>{{ $title }}</div>
   <div>画像</div>
   <div>{{ $content }}</div>
-  <div>{{ $message ?? '' }}</div>
+  <div>{{ $message }}</div>
 </div>
