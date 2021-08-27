5b74ce5254922453516c856e648cec0555bea89b
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-08-27 23:24:04 +0900

    クラスベースコンポーネントの属性・初期値を設定

--- resources/views/component/test2.blade.php
+++ resources/views/component/test2.blade.php
@@ -1,5 +1,8 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー</x-slot>
   コンポーネントテスト2
-  <x-test_class_base />
+  <br>
+  <x-test_class_base classBaseMessage="メッセージです" />
+  <br>
+  <x-test_class_base classBaseMessage="メッセージです" defaultMessage="初期値から変更"/>
 </x-tests.app>
