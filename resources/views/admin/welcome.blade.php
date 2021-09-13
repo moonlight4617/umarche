678f20d8b9bbac04d1e6032260745f710934350f
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-09-13 22:47:42 +0900

    user, admin. ownerそれぞれのnavigation.blade作成

--- resources/views/admin/welcome.blade.php
+++ resources/views/admin/welcome.blade.php
@@ -24,7 +24,7 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('admin.login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth('admins')
+                    @auth('admin')
                         <a href="{{ url('/admin/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
                         <a href="{{ route('admin.login') }}" class="text-sm text-gray-700 underline">Log in</a>
