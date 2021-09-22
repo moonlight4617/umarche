cbc4bd9ea332082fdebe7d46969685ea45c2090d
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-09-22 21:35:05 +0900

    admin/owners/create.blade.phpの作成、index.blade.phpの修正

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -23,20 +23,8 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $date_now = Carbon::now()->year;
-        $date_parse = Carbon::parse(now());
-        echo $date_now;
-        echo $date_parse;
-
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
-        // $q_first = DB::table('owners')->select('name')->first();
-
-        // $c_test = collect([
-        //     'name' => 'てすと'
-        // ]);
-        // dd($e_all, $q_get, $q_first, $c_test);
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -46,7 +34,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
