dd8c8b784cdeb760c6675152f2697e0036e3296e
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-10-16 19:17:24 +0900

    shopController updateメソッドの際のフラッシュメッセージ作成

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -15,8 +15,34 @@
                         <div class="-m-2">
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
+                                    <label for="name" class="leading-7 text-sm text-gray-600">店名 ＊必須</label>
+                                    <input type="text" id="name" name="name" value="{{ $shop->name }}" class="required w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="information" class="leading-7 text-sm text-gray-600">店舗情報 ＊必須</label>
+                                    <textarea type="textarea" id="information" name="information" rows="10" class="required w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $shop->information }}</textarea>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <div class="w-32">
+                                    <x-shop-thumbnail :filename="$shop->filename"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
                                     <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                                     <input type="file" id="image" name="image"  accept="image/png, image/jpeg, image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative flex justify-around">
+                                    <div><input type="radio" name="is_selling" value="1" class="mr-2" @if($shop->is_selling === 1){ checked } @endif>販売中</div>
+                                    <div>
+                                    <input type="radio" name="is_selling" value="0" class="mr-2" @if($shop->is_selling === 0){ checked } @endif>停止中</div>
                                 </div>
                             </div>
                         </div>
--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <x-flash-message status="session('info')" />
                     @foreach ($shops as $shop)
                     <div class="w-1/2 p-4">
                     <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
@@ -21,13 +22,7 @@
                             @endif
                             </div>
                         <div class="text-xl">{{ $shop->name }}</div>
-                        <div>
-                            @if (empty($shop->filename))
-                                <img src="{{asset('images/no_image.jpg')}}">
-                            @else 
-                                <img src="{{ asset('strage/shops/'. $shop->filename)}}">  
-                            @endif
-                        </div>
+                        <x-shop-thumbnail :filename="$shop->filename"/>
                         </div>
                     </a>
                     </div>
