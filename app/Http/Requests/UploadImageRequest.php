8a9e0c7aa5b722818269fda674b4df58ee7f222d
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-10-31 18:30:08 +0900

    image index,create

--- app/Http/Requests/UploadImageRequest.php
+++ app/Http/Requests/UploadImageRequest.php
@@ -25,6 +25,7 @@ class UploadImageRequest extends FormRequest
     {
         return [
             'image' => 'image|mimes:jpg,jpeg,png|max:2048',
+            'files.*.image' => 'required|image|mimes:jpg,jpeg,png|max:2048',
         ];
     }
 
