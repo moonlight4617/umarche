02034a526f853b99d3f973d6a8435bd59b51a97f
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-10-31 19:05:48 +0900

    store

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 
 
@@ -57,7 +58,19 @@ class ImageController extends Controller
      */
     public function store(UploadImageRequest $request)
     {
-        dd($request);
+        $imageFiles = $request->file('files');
+        if (!is_null($imageFiles)) {
+            foreach ($imageFiles as $imageFile) {
+                $fileNameToStore = ImageService::upload($imageFile, 'products');
+                Image::create([
+                    'owner_id' => Auth::id(),
+                    'filename' => $fileNameToStore
+                ]);
+            }
+        }
+        return redirect()
+            ->route('owner.images.index')
+            ->with(['message' => '画像登録しました。', 'status' => 'info']);
     }
 
     /**
--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -9,10 +9,17 @@ class ImageService
 {
   public static function upload($imageFile, $folderName)
   {
+    if (is_array($imageFile)) {
+      $file = $imageFile['image'];
+    } else {
+      $file = $imageFile;
+    }
+
+
     $fileName = uniqid(rand() . '_');
-    $extension = $imageFile->extension();
+    $extension = $file->extension();
     $fileNameToStore = $fileName . '.' . $extension;
-    $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+    $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
     Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
     return $fileNameToStore;
   }
