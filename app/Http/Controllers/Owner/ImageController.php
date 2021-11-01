c0d2d59b8c59190253457451692dc34fd8cc65f2
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-11-01 23:52:44 +0900

    edit, update

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -13,108 +13,94 @@ use App\Services\ImageService;
 
 class ImageController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:owners');
+	public function __construct()
+	{
+		$this->middleware('auth:owners');
 
-        $this->middleware(function ($request, $next) {
-            $id = $request->route()->parameter('image');
-            if (!is_null($id)) {
-                $imagesOwnerId = Image::findOrFail($id)->owner->id;
-                $imageId = (int)$imagesOwnerId;
-                if ($imageId !== Auth::id()) {
-                    abort(404);
-                }
-            }
-            return $next($request);
-        });
-    }
+		$this->middleware(function ($request, $next) {
+			$id = $request->route()->parameter('image');
+			if (!is_null($id)) {
+				$imagesOwnerId = Image::findOrFail($id)->owner->id;
+				$imageId = (int)$imagesOwnerId;
+				if ($imageId !== Auth::id()) {
+					abort(404);
+				}
+			}
+			return $next($request);
+		});
+	}
 
 
-    public function index()
-    {
-        $images = Image::where('owner_id', Auth::id())
-            ->orderBy('updated_at', 'desc')
-            ->paginate(20);
+	public function index()
+	{
+		$images = Image::where('owner_id', Auth::id())
+			->orderBy('updated_at', 'desc')
+			->paginate(20);
 
-        return view('owner.images.index', compact('images'));
-    }
+		return view('owner.images.index', compact('images'));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('owner.images.create');
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('owner.images.create');
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(UploadImageRequest $request)
-    {
-        $imageFiles = $request->file('files');
-        if (!is_null($imageFiles)) {
-            foreach ($imageFiles as $imageFile) {
-                $fileNameToStore = ImageService::upload($imageFile, 'products');
-                Image::create([
-                    'owner_id' => Auth::id(),
-                    'filename' => $fileNameToStore
-                ]);
-            }
-        }
-        return redirect()
-            ->route('owner.images.index')
-            ->with(['message' => '画像登録しました。', 'status' => 'info']);
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(UploadImageRequest $request)
+	{
+		$imageFiles = $request->file('files');
+		if (!is_null($imageFiles)) {
+			foreach ($imageFiles as $imageFile) {
+				$fileNameToStore = ImageService::upload($imageFile, 'products');
+				Image::create([
+					'owner_id' => Auth::id(),
+					'filename' => $fileNameToStore
+				]);
+			}
+		}
+		return redirect()
+			->route('owner.images.index')
+			->with(['message' => '画像登録しました。', 'status' => 'info']);
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	public function edit($id)
+	{
+		$image = Image::findOrFail($id);
+		return view('owner.images.edit', compact('image'));
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+	public function update(Request $request, $id)
+	{
+		$request->validate([
+			'title' => 'string|max:50',
+		]);
+		$image = Image::findOrFail($id);
+		$image->title = $request->title;
+		$image->save();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+		return redirect()
+			->route('owner.images.index')
+			->with(['message' => '画像を更新しました。', 'status' => 'info']);
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -1,35 +1,35 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Dashboard') }}
+    </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <x-flash-message status="session('info')" />
-                    <div class="flex justify-end mb-4">
-                        <button onclick="location.href='{{ route('owner.images.create')}}'" class="text-white bg-indigo-500 border-0 py-2 md:px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録</button>
-                    </div>
-                    @if(isset($images))
-                    <div class="flex flex-wrap">
-                    @foreach ($images as $image)
-                    <div class="w-1/4 p-4">
-                    <a href="{{ route('owner.images.edit', ['image' => $image->id]) }}">
-                        <div class="border rounded-md p-4">
-                        <div class="text-xl">{{ $image->title }}</div>
-                        <x-thumbnail :filename="$image->filename" type="products"/>
-                        </div>
-                    </a>
-                    </div>
-                    @endforeach
-                    </div>
-                    {{ $images->links() }}
-                    @endif
-                </div>
-            </div>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white border-b border-gray-200">
+          <x-flash-message status="session('info')" />
+          <div class="flex justify-end mb-4">
+            <button onclick="location.href='{{ route('owner.images.create')}}'" class="text-white bg-indigo-500 border-0 py-2 md:px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録</button>
+          </div>
+          @if(isset($images))
+          <div class="flex flex-wrap">
+          @foreach ($images as $image)
+          <div class="w-1/4 p-2 md:p-4">
+            <a href="{{ route('owner.images.edit', ['image' => $image->id]) }}">
+              <div class="border rounded-md p-2 md:p-4">
+                <x-thumbnail :filename="$image->filename" type="products"/>
+                <div class="text-xl">{{ $image->title }}</div>
+              </div>
+            </a>
+          </div>
+          @endforeach
+          </div>
+          {{ $images->links() }}
+          @endif
         </div>
+      </div>
     </div>
+  </div>
 </x-app-layout>
