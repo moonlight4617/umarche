3551e715f8d22dfd91ef60f69b67414e6280dfde
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-11-03 18:54:22 +0900

    CategorySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
