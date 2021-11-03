614f9a11b034b93ccc34164c51025b4910cc2c77
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-11-04 08:24:59 +0900

    productの初期設定

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ImageSeeder extends Seeder
 {
@@ -13,6 +14,32 @@ class ImageSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('images')->insert([
+            [
+                'owner_id' => 1,
+                'filename' => "sample1.jpg",
+                'title' => null
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => "sample2.jpg",
+                'title' => null
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => "sample3.jpg",
+                'title' => null
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => "sample4.jpg",
+                'title' => null
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => "sample5.jpg",
+                'title' => null
+            ]
+        ]);
     }
 }
--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -7,11 +7,6 @@ use Illuminate\Support\Facades\DB;
 
 class ShopSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         DB::table('shops')->insert([
