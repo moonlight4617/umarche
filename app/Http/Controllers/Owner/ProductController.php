b846709bb267dbe9b26ee4d2441699d07059a171
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-12-28 09:40:09 +0900

    product store

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Owner;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Image;
+use Illuminate\Support\Facades\DB; //クエリビルダ
 use App\Models\Product;
 use App\Models\Owner;
 use App\Models\PrimaryCategory;
+use App\Models\Stock;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 
@@ -33,7 +35,9 @@ class ProductController extends Controller
 
     public function index()
     {
-        $ownerInfo = Owner::findOrFail(Auth::id())->shop->product;
+        $ownerInfo = Owner::with('shop.product.imageFirst')
+            ->where('id', Auth::id())->get();
+
         // $ownerInfo = Owner::with('shop.product.imageFirst')
         // ->where('id', Auth::id())->get();
         return view('owner.products.index', compact('ownerInfo'));
@@ -69,7 +73,61 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request);
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'information' => 'required|string|max:1000',
+            'price' =>
+            'required|integer',
+            'sort_order' =>
+            'nullable|integer',
+            'quantity' =>
+            'required|integer',
+            'shop_id' =>
+            'required|exists:shops,id',
+            'category' =>
+            'required|exists:secondary_categories,id',
+            'image1' =>
+            'nullable|exists:images,id',
+            'image2' =>
+            'nullable|exists:images,id',
+            'image3' =>
+            'nullable|exists:images,id',
+            'image4' =>
+            'nullable|exists:images,id',
+            'is_selling' =>
+            'required',
+        ]);
+
+        try {
+            DB::transaction(function () use ($request) {
+                $product = Product::create([
+                    'name' => $request->name,
+                    'information' => $request->information,
+                    'price' => $request->price,
+                    'sort_order' => $request->sort_order,
+                    'shop_id' => $request->shop_id,
+                    'secondary_category_id' => $request->category,
+                    'image1' => $request->image1,
+                    'image2' => $request->image2,
+                    'image3' => $request->image3,
+                    'image4' => $request->image4,
+                    'is_selling' => $request->is_selling,
+                ]);
+
+                Stock::create([
+                    'product_id' => $product->id,
+                    'type' => 1,
+                    'quantity' => $request->quantity
+                ]);
+            }, 2);
+        } catch (Throwable $e) {
+            Log::error($e);
+            throw $e;
+        }
+
+        return redirect()
+            ->route('owner.products.index')
+            ->with(['message' => '商品登録しました', 'status' => 'info']);
     }
 
     /**
