30bc2e1852fbb25b2829df1581e96fc1cd310ce2
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-12-30 10:16:58 +0900

    product factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Stock;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,5 +25,7 @@ class DatabaseSeeder extends Seeder
             // StockSeeder::class,
             UserSeeder::class,
         ]);
+        Product::factory(100)->create();
+        Stock::factory(100)->create();
     }
 }
