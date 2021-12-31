abdda088e5e91b5d69831f6b8bf88c400e9b4733
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-12-31 16:42:53 +0900

    cart add

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\User\ItemController;
+use App\Http\Controllers\User\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,10 @@ Route::get('/', function () {
 Route::middleware('auth:users')->group(function () {
     Route::get('/', [ItemController::class, 'index'])->name('items.index');
     Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
+});
+
+Route::prefix('cart')->middleware('auth:users')->group(function () {
+    Route::post('add', [CartController::class, 'add'])->name('cart.add');
 });
 
 // Route::get('/dashboard', function () {
