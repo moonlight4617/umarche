4070261c1412c118d0d2f78fce1a0c11359c3dbd
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-12-31 10:24:27 +0900

    cartのリレーション設定

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,7 @@ use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
 use App\Models\Stock;
+use App\Models\User;
 
 class Product extends Model
 {
@@ -60,5 +61,11 @@ class Product extends Model
     public function stock()
     {
         return $this->hasMany(Stock::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'carts')
+            ->withPivot(['id', 'quantity']);
     }
 }
