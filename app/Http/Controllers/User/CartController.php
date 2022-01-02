5e924f65d31ff92f34fd1628bdc48419ea50bc08
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-01-02 11:15:45 +0900

    決済後のカート内削除

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -91,12 +91,19 @@ class CartController extends Controller
             'payment_method_types' => ['card'],
             'line_items' => [$lineItems],
             'mode' => 'payment',
-            'success_url' => route('user.items.index') . '/success.html',
-            'cancel_url' => route('user.cart.index') . '/cancel.html',
+            'success_url' => route('user.cart.success'),
+            'cancel_url' => route('user.cart.index'),
         ]);
 
         $publicKey = env('STRIPE_PUBLIC_KEY');
 
         return view('user.checkout', compact('session', 'publicKey'));
+    }
+
+    public function success()
+    {
+        Cart::where('user_id', Auth::id())->delete();
+
+        return redirect()->route('user.items.index');
     }
 }
