6cf30845efe5d1479a1664320415910be4b5887f
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-01-04 09:47:58 +0900

    キーワード検索機能実装

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -36,6 +36,7 @@ class ItemController extends Controller
 
         $products = Product::availableItems()
             ->selectCategory($request->category ?? '0')
+            ->searchKeyword($request->keyword)
             ->sortOrder($request->sort)
             ->paginate($request->pagination ?? '20');
         return view('user.index', compact('products', 'categories'));
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -118,8 +118,23 @@ class Product extends Model
 
     public function scopeSelectCategory($query, $categoryId)
     {
-        if ($categoryId !== 0) {
+        if ($categoryId != 0) {
             return $query->where('secondary_category_id', $categoryId);
+        } else {
+            return;
+        }
+    }
+
+    public function scopeSearchKeyword($query, $keyword)
+    {
+        if (!is_null($keyword)) {
+            $spaceConvert = mb_convert_kana($keyword, 's'); //全角スペースを半角に
+            $keywords = preg_split('/[\s]+/', $spaceConvert, -1, PREG_SPLIT_NO_EMPTY); //空白で区切る
+            foreach ($keywords as $word) //単語をループで回す
+            {
+                $query->where('products.name', 'like', '%' . $word . '%');
+            }
+            return $query;
         } else {
             return;
         }
